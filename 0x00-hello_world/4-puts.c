561b9c1aaa8c1041ca70661d18455444876a7b32
Edwardrem <rufaromutepfa@gmail.com>
2023-06-15 15:30:06 -0700

    a C program that prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int main(void) {
-	  /* This function prints the string "Programming is like building a multilingual puzzle." to the standard output. */
+int main(void)
+{
+	  /* This function prints the string to the standard output. */
 	  puts("Programming is like building a multilingual puzzle.");
 	    return 0;
 }
