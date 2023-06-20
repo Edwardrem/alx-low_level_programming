0d7aaae02980d1b1e3b77bd61368baff9429d880
Edwardrem <rufaromutepfa@gmail.com>
2023-06-20 16:27:56 -0700

    a program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,3 @@
-#include <stdio.h>
-
-/**main - Check point
- *
- * Return: Always 0 (Success)
- */
-#include "main.h"
-int main(void)
-{
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
-	return (0);
+void _putchar(char c) {
+	  putchar(c);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,21 @@
-void _putchar(char c) {
-	  putchar(c);
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Retur: Always 0 (Success)
+ */
+#include "main.h"
+int main(void)
+{
+	putchar('-');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
 }
