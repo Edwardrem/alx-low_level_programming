15e07a6f740332a07a8f3481042b359a108dd2b4
Rufaro Edward Mutepfa <45569487+Edwardrem@users.noreply.github.com>
2023-06-21 15:18:51 +0200

    Update 100-times_table.c
    a function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,49 @@
 #include "main.h"
 
 /**
+ * print_spaces - prints a given number of spaces
+ * @n: the number of spaces to print
+ *
+ * Return: void
+ */
+void print_spaces(int n)
+{
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        _putchar(' ');
+    }
+}
+
+/**
+ * print_number - prints an integer using _putchar
+ * @n: the integer to print
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+    if (n >= 100)
+    {
+        _putchar(n / 100 + '0');
+        _putchar((n / 10) % 10 + '0');
+        _putchar(n % 10 + '0');
+    }
+    else if (n >= 10)
+    {
+        _putchar(' ');
+        _putchar(n / 10 + '0');
+        _putchar(n % 10 + '0');
+    }
+    else
+    {
+        print_spaces(2);
+        _putchar(n + '0');
+    }
+}
+
+/**
  * print_times_table - prints the n times table
  * @n: the times table to print
  *
@@ -8,46 +51,25 @@
  */
 void print_times_table(int n)
 {
-	int i;
-	int j;
-	int product;
+    int i;
+    int j;
+    int product;
 
-	if (n >= 0 && n <= 15)
-	{
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
-				product = i * j;
-				if (j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-				if (product < 10 && j != 0)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(product + '0');
-				}
-				else if (product >= 10 && product < 100)
-				{
-					_putchar(' ');
-					_putchar(product / 10 + '0');
-					_putchar(product % 10 + '0');
-				}
-				else if (product >= 100)
-				{
-					_putchar(product / 100 + '0');
-					_putchar((product / 10) % 10 + '0');
-					_putchar(product % 10 + '0');
-				}
-				else
-				{
-					_putchar(product + '0');
-				}
-			}
-			_putchar('\n');
-		}
-	}
+    if (n >= 0 && n <= 15)
+    {
+        for (i = 0; i <= n; i++)
+        {
+            for (j = 0; j <= n; j++)
+            {
+                product = i * j;
+                if (j != 0)
+                {
+                    _putchar(',');
+                    _putchar(' ');
+                }
+                print_number(product);
+            }
+            _putchar('\n');
+        }
+    }
 }
