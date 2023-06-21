75c95b587a3f7e89484e649a3d1eec33fdc228dc
Edwardrem <rufaromutepfa@gmail.com>
2023-06-21 09:23:07 -0700

    a function that prints the n times table, starting with 0.a function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,75 +1,30 @@
 #include "main.h"
 
 /**
- * print_spaces - prints a given number of spaces
- * @n: the number of spaces to print
- *
- * Return: void
- */
-void print_spaces(int n)
-{
-    int i;
-
-    for (i = 0; i < n; i++)
-    {
-        _putchar(' ');
-    }
-}
-
-/**
- * print_number - prints an integer using _putchar
- * @n: the integer to print
- *
- * Return: void
- */
-void print_number(int n)
-{
-    if (n >= 100)
-    {
-        _putchar(n / 100 + '0');
-        _putchar((n / 10) % 10 + '0');
-        _putchar(n % 10 + '0');
-    }
-    else if (n >= 10)
-    {
-        _putchar(' ');
-        _putchar(n / 10 + '0');
-        _putchar(n % 10 + '0');
-    }
-    else
-    {
-        print_spaces(2);
-        _putchar(n + '0');
-    }
-}
-
-/**
- * print_times_table - prints the n times table
- * @n: the times table to print
- *
- * Return: void
- */
+ *  * print_times_table - prints the n times table
+ *   * @n: the times table to print
+ *    *
+ *     * Return: void
+ *      */
 void print_times_table(int n)
 {
-    int i;
-    int j;
-    int product;
+	int i;
+	int j;
 
-    if (n >= 0 && n <= 15)
-    {
-        for (i = 0; i <= n; i++)
-        {
-            for (j = 0; j <= n; j++)
-            {
-                product = i * j;
-                if (j != 0)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                }
-                print_number(product);
-            }
-            _putchar('\n');
-        }
-    }
+	if (n > 15 || n < 0)
+	{
+		return;
+	}
+	for (i = 0; i <= n; j++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			_putchar('0' + i * j);
+			if (j < n)
+			{
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 }
