a102137227e73e4bd3ab541d5e802593d69f8594
Edwardrem <rufaromutepfa@gmail.com>
2023-07-05 08:41:27 -0700

    a function that prints a buffer.

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * print_buffer - prints a buffer
+ * @b: pointer to the buffer to print
+ * @size: the number of bytes to print
+ *
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+
+	for (j = 0; j < 10; j++)
+	{
+		if (i + j < size)
+		{
+			printf("%02x", b[i + j]);
+		}
+		else
+		{
+			printf("  ");
+		}
+
+		if (j % 2)
+		{
+			_putchar(' ');
+		}
+	}
+
+	for (j = 0; j < 10; j++)
+	{
+		if (i + j < size)
+		{
+			_putchar(b[i + j] >= ' ' && b[i + j] <= '~' ? b[i + j] : '.');
+		}
+	}
+
+	_putchar('\n');
+	}
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
