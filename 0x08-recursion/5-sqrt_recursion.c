de0b2b0d20f7ba9c3ab6f65abcb3d63369fe5eec
Edwardrem <rufaromutepfa@gmail.com>
2023-07-05 07:39:36 -0700

    a function that returns the natural square root of a number.

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion_helper - helper function to find the natural square root
+ * @n: the number to find the square root of
+ * @i: the current divisor to check
+ *
+ * Return: the natural square root of n,
+ */
+int _sqrt_recursion_helper(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	if (i * i == n)
+	{
+		return (i);
+	}
+	return (_sqrt_recursion_helper(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to find the square root of
+ *
+ * Return: the natural square root of n,
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (_sqrt_recursion_helper(n, 0));
+}
