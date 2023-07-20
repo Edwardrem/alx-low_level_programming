ec8870cd7ec725554355193f6bb403c149fb29b8
Edwardrem <rufaromutepfa@gmail.com>
2023-07-20 01:36:15 -0700

    a function that creates a new dog.

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,68 @@
+#include "dog.h"
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: string to duplicate
+ *
+ * Return: pointer to new string
+ */
+char *_strdup(char *str)
+{
+	char *new_str;
+	int i, len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		len++;
+
+	new_str = malloc(sizeof(char) * (len + 1));
+
+	if (new_str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		new_str[i] = str[i];
+
+	new_str[len] = '\0';
+
+	return (new_str);
+}
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: pointer to new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+
+	new_dog->name = _strdup(name);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+
+	new_dog->age = age;
+
+	new_dog->owner = _strdup(owner);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+
+	return (new_dog);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -24,6 +24,8 @@ typedef struct dog dog_t;
 
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+char *_strdup(char *str);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* DOG_H */
 
